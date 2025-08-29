a849e72d112425e42c0a7560821a112c4c65cca6
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-29 11:14:38 +0700

    update home: add hero section ui

--- components/layouts/Navbar.tsx
+++ components/layouts/Navbar.tsx
@@ -1,77 +1,77 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import {
     NavigationMenu,
     NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
     NavigationMenuLink,
     NavigationMenuList,
     NavigationMenuTrigger,
     navigationMenuTriggerStyle,
-    NavigationMenuViewport,
 } from "@/components/ui/navigation-menu"
 
 import { Button } from "../ui/button";
 import EachUtils from "@/utils/EachUtils";
 import { LIST_NAVBAR } from "@/constants/listNavbar";
-import Link from "next/link";
 
 export function Navbar() {
     return (
-        <nav className="w-full fixed py-8 px-6 sm:px-8 flex items-center justify-between">
-            <Image
-                src="/logo.png"
-                alt="Logo"
-                width={100}
-                height={100}
-                priority
-            />
+        <header className="w-full fixed py-8 px-6 sm:px-8 z-50">
+            <nav className="flex items-center justify-between border-b-[1px] pb-4">
+                <Image
+                    src="/logo.png"
+                    alt="Logo"
+                    width={100}
+                    height={100}
+                    priority
+                />
 
-            <NavigationMenu viewport={false}>
-                <NavigationMenuList>
-                    <EachUtils 
-                        of={LIST_NAVBAR}
-                        render={(item, index) => (
-                            <NavigationMenuItem key={index}>
-                                {item.url ? (
-                                    <NavigationMenuLink 
-                                        asChild 
-                                        className={navigationMenuTriggerStyle()}
-                                    >
-                                        <Link href={item.url}>{item.title}</Link>
-                                    </NavigationMenuLink>
-                                ) : (
-                                    <>
-                                        <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
-                                        <NavigationMenuContent>
-                                            <ul className="grid w-[200px] gap-2 p-3">
-                                                <EachUtils 
-                                                    of={item.subMenu ?? []}
-                                                    render={(item, index) => (
-                                                        <li key={index}>
-                                                            <NavigationMenuLink asChild>
-                                                                <Link 
-                                                                    href={item.url} 
-                                                                    className="block px-2 py-1 rounded-md hover:bg-gray-100 transition"
-                                                                >
-                                                                    {item.title}
-                                                                </Link>
-                                                            </NavigationMenuLink>
-                                                        </li>
-                                                    )}
-                                                />
-                                            </ul>
-                                        </NavigationMenuContent>
-                                    </>
-                                )}
-                            </NavigationMenuItem>
-                        )}
-                    />
-                </NavigationMenuList>
-            </NavigationMenu>
+                <NavigationMenu viewport={false}>
+                    <NavigationMenuList>
+                        <EachUtils 
+                            of={LIST_NAVBAR}
+                            render={(item, index) => (
+                                <NavigationMenuItem key={index}>
+                                    {item.url ? (
+                                        <NavigationMenuLink 
+                                            asChild 
+                                            className={navigationMenuTriggerStyle()}
+                                        >
+                                            <Link href={item.url}>{item.title}</Link>
+                                        </NavigationMenuLink>
+                                    ) : (
+                                        <>
+                                            <NavigationMenuTrigger>{item.title}</NavigationMenuTrigger>
+                                            <NavigationMenuContent>
+                                                <ul className="grid w-[200px] gap-2 p-3">
+                                                    <EachUtils 
+                                                        of={item.subMenu ?? []}
+                                                        render={(item, index) => (
+                                                            <li key={index}>
+                                                                <NavigationMenuLink asChild>
+                                                                    <Link 
+                                                                        href={item.url} 
+                                                                        className="block px-2 py-1 rounded-md hover:bg-gray-100 transition"
+                                                                    >
+                                                                        {item.title}
+                                                                    </Link>
+                                                                </NavigationMenuLink>
+                                                            </li>
+                                                        )}
+                                                    />
+                                                </ul>
+                                            </NavigationMenuContent>
+                                        </>
+                                    )}
+                                </NavigationMenuItem>
+                            )}
+                        />
+                    </NavigationMenuList>
+                </NavigationMenu>
 
-            <Button>Login</Button>
-        </nav>
+                <Button className="px-5 bg-transparent border-black text-black hover:bg-green-800 hover:text-white hover:border-transparent border rounded-full cursor-pointer">Login</Button>
+            </nav>
+        </header>
     )
 }
--- components/modules/home/HeroSection.tsx
+++ components/modules/home/HeroSection.tsx
@@ -1,7 +1,21 @@
+import { Button } from "@/components/ui/button";
+
 export function HeroSection() {
     return (
-        <section className="w-full h-screen flex items-center justify-center">
-            <h1 className="text-4xl font-bold">Ubah Sampah Menjadi Manfaat</h1>
+        <section className="w-full h-screen flex flex-col items-center justify-center px-4">
+            <div className="max-w-4xl mt-14 text-center">
+                {/* <p className="text-lg font-medium text-green-600 tracking-wide uppercase">Sampah Berkurang, Manfaat Bertambah</p> */}
+                <div className="relative w-fit mx-auto mb-2 rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                    Sampah Berkurang, Manfaat Bertambah
+                </div>
+
+                <h1 className="text-4xl font-extrabold tracking-tight sm:text-6xl text-gray-900 leading-tight">Reuse, Reduce, Recycle <br /> untuk Kehidupan Lebih Baik</h1>
+
+                <p className="text-base sm:text-lg text-gray-600 leading-relaxed mt-5">Kami percaya bahwa setiap sampah memiliki potensi untuk menjadi sesuatu yang bermanfaat. 
+                Dengan mengurangi, menggunakan kembali, dan mendaur ulang, kita bersama-sama bisa menciptakan lingkungan yang lebih bersih, sehat, dan berkelanjutan.</p>
+
+                <Button className="mt-5">Pelajari Lebih Lanjut</Button>
+            </div>
         </section>
     )
 }
