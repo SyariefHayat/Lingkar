1e55f4051328807cf0fadcabb9a17b9cdc06f608
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-30 07:15:21 +0700

    update frontend: add value section

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/modules/home/HeroSection";
+import ValueSection from "@/components/modules/home/ValueSection";
 
 export default function Home() {
     return (
         <div className="w-full h-full font-poppins">
             <HeroSection />
+            <ValueSection />
         </div>
     );
 }
---
+++ components/modules/home/ValueSection.tsx
@@ -0,0 +1,11 @@
+export default function ValueSection() {
+    return (
+        <section className="w-full px-8">
+            <h2 className="text-2xl sm:text-3xl md:text-4xl font-medium text-gray-900 leading-relaxed tracking-tight">
+                Transparansi, integritas, dan profesionalisme membimbing setiap langkah kami. 
+                Lewat informasi yang terbuka dan solusi yang jujur, kami mengajak masyarakat 
+                untuk bersama-sama mengambil keputusan bijak dalam mengelola sampah.
+            </h2>
+        </section>
+    )
+}
