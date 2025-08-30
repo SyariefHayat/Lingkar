d411976a1fc4825077a17bb80099a2727f36501d
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-30 14:26:38 +0700

    update frontend: what we do padding section

--- components/modules/home/WhatWeDoSection.tsx
+++ components/modules/home/WhatWeDoSection.tsx
@@ -10,15 +10,15 @@ export default function WhatWeDoSection() {
     const [active, setActive] = useState(1);
 
     return (
-        <section className="w-full min-h-screen sm:h-screen px-8 py-16 bg-white">
+        <section className="w-full min-h-screen lg:h-screen px-8 py-5 sm:py-16 bg-white">
             <div className="flex items-center gap-3 mb-8 md:mb-10">
                 <div className="w-4 h-4 sm:w-5 sm:h-5 rounded-full border-2"></div>
-                <h3 className="text-lg sm:text-xl font-semibold text-gray-600">
+                <h3 className="text-2xl sm:text-3xl md:text-4xl font-semibold">
                     Apa yang Kami Lakukan ?
                 </h3>
             </div>
 
-            <div className="flex flex-col md:flex-row w-full h-full border rounded-xl">
+            <div className="flex flex-col md:flex-row w-full h-full border">
                 <EachUtils 
                     of={LIST_WHAT_WE_DO}
                     render={(item, index) => (
@@ -28,7 +28,7 @@ export default function WhatWeDoSection() {
                             animate={{ flex: active === item.id ? 4 : 1 }}
                             transition={{ duration: 0.5, ease: "easeInOut" }}
                             className={`
-                                border-t md:border-t-0 md:border-l 
+                                ${item.id !== 1 ? "md:border-l border-t" : "md:border-t-0"} 
                                 p-4 sm:p-6 flex flex-col justify-start text-left cursor-pointer
                                 ${active === item.id ? "bg-green-50" : "bg-white"}
                             `}
