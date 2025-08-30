25bbe65882358e5ad17c39d09b4014b8231a3843
SyariefHayat <muhammadsyarifhayat@gmail.com>
2025-08-30 13:36:58 +0700

    update frontend: hero and value padding section

--- components/modules/home/ValueSection.tsx
+++ components/modules/home/ValueSection.tsx
@@ -1,8 +1,8 @@
 export default function ValueSection() {
     return (
-        <section className="w-full px-8">
+        <section className="w-full px-8 py-5 sm:py-16">
             <h2 className="text-2xl sm:text-3xl md:text-4xl font-medium text-gray-900 leading-relaxed tracking-tight">
-                Transparansi, integritas, dan profesionalisme membimbing setiap langkah kami. 
+                <span className="text-green-500">Transparansi, integritas, dan profesionalisme</span> membimbing setiap langkah kami. 
                 Lewat informasi yang terbuka dan solusi yang jujur, kami mengajak masyarakat 
                 untuk bersama-sama mengambil keputusan bijak dalam mengelola sampah.
             </h2>
